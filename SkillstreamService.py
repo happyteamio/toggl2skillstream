22bc4fc52ec3fd7136acbc71e52a62683054c3a6
rafek <rafal.legiedz@gmail.com>
2015-10-07 05:25:21 +0200

    Moving more stuff to SkillstreamService. Refactoring.

--- SkillstreamService.py
+++ SkillstreamService.py
@@ -1,10 +1,25 @@
 import sys
+import ConfigParser
+from pyquery import PyQuery as pq
 
 class SkillstreamService:
+
+    def __init__(self):
+        config = ConfigParser.ConfigParser()
+        config.read("config.ini")
+
+        self.host = config.get("DEFAULT", "SHost")
+        self.path = config.get("DEFAULT", "SPath")
+        self.username = config.get("DEFAULT", "SUsername")
+        self.password = config.get("DEFAULT", "SPassword")
+
+        self.base_url = "https://" + self.host + self.path
 
     def authorize(func):
         def inner(*args, **kwargs):
             print "Logging in.."
+
+            print args[0].get_header()
 
             try:
                 func(*args, **kwargs)
@@ -17,4 +32,14 @@ class SkillstreamService:
 
     @authorize
     def report_hours(self, date, hours, minutes):
-        raise NotImplementedError
+
+        print "Reporting hours.."
+
+    def get_header(self):
+        return {
+            "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:41.0) Gecko/20100101 Firefox/41.0",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+            "Accept-Language": "pl,en-US;q=0.7,en;q=0.3",
+            "Accept-Encoding": "gzip, deflate",
+            "Connection": "keep-alive"
+        }
